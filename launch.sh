f421c4a5053480d17d4ce2a91aaa7979571048ed
hypothermic <admin@hypothermic.nl>
2018-10-08 21:25:07 +0200

    Fix posix permissions

--- launch.sh
+++ launch.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 /usr/bin/script -q -c "/usr/bin/screen ${*}" /dev/null
+/usr/bin/setfacl -R -m u:mfsrv:rwx /home/mfsrv/.mfsrv/
 /usr/bin/nohup java -jar mfsrv-*.jar -Xms16M -Xmn32M -Xmx768M </dev/null >/dev/null 2>&1 &
