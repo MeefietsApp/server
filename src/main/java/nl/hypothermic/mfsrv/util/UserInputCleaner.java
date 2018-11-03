448ddb1e521f0dd955c5038cb071aec2e1e78035
hypothermic <admin@hypothermic.nl>
2018-11-03 20:26:52 +0100

    Clean user input voor /account/manage/setname params

--- src/main/java/nl/hypothermic/mfsrv/util/UserInputCleaner.java
+++ src/main/java/nl/hypothermic/mfsrv/util/UserInputCleaner.java
@@ -2,8 +2,14 @@ package nl.hypothermic.mfsrv.util;
 
 public class UserInputCleaner {
 	
+	// FIXME: mensen kunnen nog steeds HTML entities (&lt;) gebruiken, Android TextView formatteert deze automatisch.
+	
+	public static final char[] DISALLOWED_CHARS = {'<', '>', '\\', '[', ']', '*', '+', '=', '?', '^'};
+	
 	public static String clean(String str) {
-		/// TODO voor de release!!!!
+		for (char iter : DISALLOWED_CHARS) {
+			str.replace(iter, ' ');
+		}
 		return str;
 	}
 }
